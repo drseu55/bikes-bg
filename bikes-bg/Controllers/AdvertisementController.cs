cb26cf3f240d887fd05fcdfb719d7b44bf7af5b7
Aydzhan Ahmedov <darkvigoole@gmail.com>
2021-06-05 12:27:12 +0300

    Search with filter implented

--- bikes-bg/Controllers/AdvertisementController.cs
+++ bikes-bg/Controllers/AdvertisementController.cs
@@ -11,6 +11,7 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace bikes_bg.Controllers
 {
@@ -116,6 +117,68 @@ namespace bikes_bg.Controllers
             }
 
             return View("ViewAd", model);
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            SearchAdViewModel searchAdViewModel = new SearchAdViewModel();
+
+            LoadFilterData(searchAdViewModel.searchFilter);
+            searchAdViewModel.advertisements = advertisementRepo.GetTable()
+                .Include(ad => ad.bikeModel)
+                .Include(ad => ad.bikeModel.bikeBrand)
+                .ToList();
+
+            return View(searchAdViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Search(SearchAdViewModel model)
+        {
+            model.advertisements = GetAdsByFilter(model.searchFilter);
+            LoadFilterData(model.searchFilter);
+            return View(model);
+        }
+
+        public bool LoadFilterData(AdSearchFilter searchFilter)
+        {
+            searchFilter.bikeBrands = bikeBrandRepo.GetAll().ToList();
+            searchFilter.regions = regionRepo.GetAll().ToList();
+
+            return true;
+        }
+
+        List<Advertisement> GetAdsByFilter(AdSearchFilter searchFilter)
+        {
+            var result = advertisementRepo.GetTable()
+                .Include(ad => ad.bikeModel).ThenInclude(model => model.bikeBrand)
+                .AsQueryable();
+
+            if (searchFilter != null)
+            {
+                if (searchFilter.brandId.HasValue)
+                {
+                   result = result.Where(ad => ad.bikeModel.brandID == searchFilter.brandId);
+                }
+
+                if (searchFilter.priceFrom.HasValue)
+                {
+                    result = result.Where(ad => ad.price >= searchFilter.priceFrom);
+                }
+
+                if (searchFilter.priceTo.HasValue)
+                {
+                    result = result.Where(ad => ad.price <= searchFilter.priceTo);
+                }
+
+                if (searchFilter.regionId.HasValue)
+                {
+                    result = result.Where(ad => ad.city.regionID == searchFilter.regionId);
+                }
+            }
+
+            return result.ToList();
         }
 
         [HttpPost]
---
+++ bikes-bg/ViewModels/SearchAdViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bikes_bg.Models;
+
+namespace bikes_bg.ViewModels
+{
+    public class SearchAdViewModel
+    {
+        public SearchAdViewModel()
+        {
+            searchFilter = new AdSearchFilter();
+        }
+
+        public AdSearchFilter searchFilter { get; set; }
+        public List<Advertisement> advertisements { get; set; }
+    }
+
+}
